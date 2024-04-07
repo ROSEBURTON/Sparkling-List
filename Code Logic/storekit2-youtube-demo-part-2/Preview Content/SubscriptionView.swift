7d0b6d017d242dc513a3f3404a250c7a085efe01
Rosalinda Burton <114356834+ROSEBURTON@users.noreply.github.com>
2024-04-07 07:57:43 -0600

    Add files via upload

---
+++ Code Logic/storekit2-youtube-demo-part-2/Preview Content/SubscriptionView.swift
@@ -0,0 +1,43 @@
+//
+//  SubscriptionView.swift
+//  storekit2-youtube-demo-part-2
+//
+//  Created by Paulo Orquillo on 2/03/23.
+//
+
+import SwiftUI
+import StoreKit
+
+struct SubscriptionView: View {
+    @EnvironmentObject var storeVM: StoreVM
+    @State var isPurchased = false
+
+    var body: some View {
+        ForEach(storeVM.subscriptions) { product in
+            Button(action: {
+                Task {
+                    await buy(product: product)
+                }
+            }) {
+                Text("\(product.displayPrice) \(product.description)")
+            }
+        }
+    }
+
+    
+    func buy(product: Product) async {
+        do {
+            if try await storeVM.purchase(product) != nil {
+                isPurchased = true
+            }
+        } catch {
+            print("purchase failed")
+        }
+    }
+}
+
+struct SubscriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubscriptionView().environmentObject( StoreVM())
+    }
+}
