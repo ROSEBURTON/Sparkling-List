25b7e1010ad3ba2d22f2e6b52cf1bb773317a53e
Rosalinda Burton <114356834+ROSEBURTON@users.noreply.github.com>
2024-06-02 12:59:58 -0600

    Add files via upload

---
+++ Sparkling Logic/TEST.swift
@@ -0,0 +1,19 @@
+//
+//  TEST.swift
+//  Sparkling List
+//
+//  Created by IAL VECTOR on 5/24/24.
+//
+
+import SwiftUI
+import CreateML
+
+struct TEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TEST()
+}
